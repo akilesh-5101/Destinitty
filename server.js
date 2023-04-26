9adde0983df9f7a82ebfc7c897fd2cd3e57e28d0
zaindeen <m.s.zainulabideen@gmail.com>
2023-04-27 01:44:39 +0530

    nightCommit

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const authenticationRouter = require('./routes/authentication');
 const app = express();
 
 app.listen(5000);
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/Assets'));
 
-app.get('/',async (req,res) => {
+app.get('/',(req,res) => {
 	// const articles = await Article.find().sort({createdAt: 'desc'});
 	res.render('index');
 });
+
+app.use('/account', authenticationRouter);
