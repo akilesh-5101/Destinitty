661238255f4e36c15ea727f61a2b97d1ebcf02b0
zaindeen <m.s.zainulabideen@gmail.com>
2023-04-28 10:04:26 +0530

    upto signin

---
+++ Models/UserMongo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    pass:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Authentication', Schema);
--- routes/authentication.js
+++ routes/authentication.js
@@ -2,8 +2,4 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/register', (req,res) => {
-    res.render('authenticate/register');//
-})
-
 module.exports = router;
